158838a2e132c552d009fa30889441167b93b97f
庄骥南 <a492073467@gmail.com>
2020-05-14 17:57:44 +0800

    能够复制普通对象、数组、函数

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 function deepClone(source) {
+    if (source instanceof Object) {
+        if (source instanceof Array) {
+            const dist = new Array();
+            for (const key in source) {
+                if (source.hasOwnProperty(key)) {
+                    dist[key] = deepClone(source[key]);
+                }
+            }
+            return dist;
+        } else if (source instanceof Function) {
+            const dist = function () {
+                return source.apply(this, arguments);
+            };
+            for (const key in source) {
+                if (source.hasOwnProperty(key)) {
+                    dist[key] = deepClone(source[key]);
+                }
+            }
+            return dist;
+        } else {
+            const dist = new Object();
+            for (const key in source) {
+                if (source.hasOwnProperty(key)) {
+                    dist[key] = deepClone(source[key]);
+                }
+            }
+            return dist;
+        }
+    }
     return source;
 }
 
--- test/index.js
+++ test/index.js
@@ -35,4 +35,40 @@ describe("deepClone", () => {
         const sym2 = deepClone(sym);
         assert(sym === sym2);
     });
+
+    describe("对象", () => {
+        it("能够复制普通对象", () => {
+            const a = { name: "john", age: 18, child: { name: "bao" } };
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert(a.name === a2.name);
+            assert(a.child !== a2.child);
+            assert(a.child.name === a2.child.name);
+        });
+        it("能够复制数组对象", () => {
+            const a = [
+                [11, 12],
+                [21, 22],
+                [31, 32],
+            ];
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert(a[0] !== a2[0]);
+            assert(a[1] !== a2[1]);
+            assert(a[2] !== a2[0]);
+            assert.deepEqual(a, a2);
+        });
+        it("能够复制函数", () => {
+            const a = function (x, y) {
+                return x + y;
+            };
+            a.xxx = { yyy: { zzz: 1 } };
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+            assert(a.xxx.yyy !== a2.xxx.yyy);
+            assert(a.xxx !== a2.xxx);
+            assert(a(1, 2) === a2(1, 2));
+        });
+    });
 });
